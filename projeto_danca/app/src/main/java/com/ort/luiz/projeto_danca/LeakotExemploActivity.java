b989ede95f2e9bb027d4c4236102a5c1b6e85b0f
Luiz-Sama <themax285gamer@gmail.com>
2018-09-16 20:25:48 -0300

    Cria o botao facebook e instagram

--- projeto_danca/app/src/main/java/com/ort/luiz/projeto_danca/LeakotExemploActivity.java
+++ projeto_danca/app/src/main/java/com/ort/luiz/projeto_danca/LeakotExemploActivity.java
@@ -1,10 +1,16 @@
 package com.ort.luiz.projeto_danca;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.IBinder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -46,19 +52,54 @@ public class LeakotExemploActivity extends AppCompatActivity {
             @Override
             public void onCancelled(DatabaseError databaseError) { }});
 
+
+        Context context = getApplicationContext();
+
         btnFacebook =  findViewById(R.id.btnFacebookId);
         btnFacebook.setOnClickListener((V)->{
+            Intent facebook = getOpenFacebookIntent(context);
+            startActivity(facebook);
 
         });
 
         btnInstagram = findViewById(R.id.btnInstagramId);
-        btnInstagram.setOnClickListener((V)->{
-
+        btnInstagram.setOnClickListener((V) ->{
+            Intent instagram  = newInstagramProfileIntent(context.getPackageManager(), "http://instagram.com/jaredrummler");
+            startActivity(instagram);
+            Toast.makeText(getApplicationContext(),"Clicou",Toast.LENGTH_SHORT).show();
         });
 
         btnInternet = findViewById(R.id.btnInternetId);
         btnInternet.setOnClickListener((V)->{
-            
+
         });
+    }
+
+    public static Intent newInstagramProfileIntent(PackageManager pm, String url) {
+        final Intent intent = new Intent(Intent.ACTION_VIEW);
+        try {
+            if (pm.getPackageInfo("com.instagram.android", 0) != null) {
+                if (url.endsWith("/")) {
+                    url = url.substring(0, url.length() - 1);
+                }
+                final String username = url.substring(url.lastIndexOf("/") + 1);
+
+                intent.setData(Uri.parse("http://instagram.com/_u/" + username));
+                intent.setPackage("com.instagram.android");
+                return intent;
+            }
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+        intent.setData(Uri.parse(url));
+        return intent;
+    }
+
+    public static Intent getOpenFacebookIntent(Context context) {
+        try {
+            context.getPackageManager().getPackageInfo("com.facebook.katana", 0);
+            return new Intent(Intent.ACTION_VIEW, Uri.parse("fb://page/hebraicario"));
+        } catch (Exception e) {
+            return new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.facebook.com/"));
+        }
     }
 }
